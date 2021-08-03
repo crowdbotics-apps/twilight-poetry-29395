c8bb9d61a78e635621a20ef2a439423daa188a34
Crowdbotics <team@crowdbotics.com>
2021-08-03 14:07:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "twilight_poetry_29395Identifier",
+  url: "https://twilight_poetry_29395Identifier.botics.co",
+  api: "https://twilight_poetry_29395Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
